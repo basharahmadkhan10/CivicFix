2e90f48de971f86a083e996aa277d238a2d0161b
Bashar Ahmad Khan <150275763+basharahmadkhan10@users.noreply.github.com>
2026-03-02 18:48:58 +0530

    Update main.jsx

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -4,12 +4,15 @@ import "./index.css";
 import { ThemeProvider } from "./context/ThemeContext";
 import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext";
+import { ToastProvider } from "./context/ToastContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <AuthProvider>
       <ThemeProvider>
-        <App />
+        <ToastProvider>
+          <App />
+        </ToastProvider>
       </ThemeProvider>
     </AuthProvider>
   </BrowserRouter>,
